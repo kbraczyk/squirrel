9f36357efc00b7360add561294ce7685667ada98
Krzysztof Brączyk <braczykkrzysztof@gmail.com>
2020-03-13 17:14:59 +0100

    Zmieniono implementacje pipe formatującego

--- src/app/common/pipe/formater.pipe.ts
+++ src/app/common/pipe/formater.pipe.ts
@@ -8,40 +8,52 @@ export class FormaterPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     const [type] = args;
 
-    if (type === 'activity') {
-      switch (value) {
-        case 1.2: {
-          return 'niska';
-        }
-        case 1.35: {
-          return 'mała';
-        }
-        case 1.55: {
-          return 'średnia';
-        }
-        case 1.75: {
-          return 'duża';
-        }
-        case 2: {
-          return 'bardzo duża';
-        }
-        default: {
-          return 'nieznana';
-        }
+    switch (type) {
+      case 'activity': {
+        return activityFormatter(value);
       }
-    } else if (type === 'target') {
-      switch (value) {
-        case 1: {
-          return 'utrzymanie wagi';
-        }
-        case 1.2: {
-          return 'podniesienie wagi';
-        }
-        default: {
-          return 'utrata wagi';
-        }
+      case 'target': {
+        return targetFormatter(value);
       }
     }
     return null;
   }
 }
+
+export const activityFormatter = (value) => {
+  switch (value) {
+    case 1.2: {
+      return 'niska';
+    }
+    case 1.35: {
+      return 'mała';
+    }
+    case 1.55: {
+      return 'średnia';
+    }
+    case 1.75: {
+      return 'duża';
+    }
+    case 2: {
+      return 'bardzo duża';
+    }
+    default: {
+      return 'nieznana';
+    }
+  }
+};
+
+export const targetFormatter = (value) => {
+  switch (value) {
+    case 1: {
+      return 'utrzymanie wagi';
+    }
+    case 1.2: {
+      return 'podniesienie wagi';
+    }
+    default: {
+      return 'utrata wagi';
+    }
+  }
+};
+
