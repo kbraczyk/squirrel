6c3d2aa9ca649596b098dbe326269fa13fa2ca13
Krzysztof Brączyk <braczykkrzysztof@gmail.com>
2020-03-14 11:21:06 +0100

    Dodano testy jednostkowe dla komponentu error-form

--- src/app/common/components/form/error-form/error-form.component.spec.ts
+++ src/app/common/components/form/error-form/error-form.component.spec.ts
@@ -1,25 +1,67 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ErrorFormComponent } from './error-form.component';
+import { ErrorFormComponent } from './error-form.component';
+import { MatFormFieldModule } from '@angular/material';
+import { FormControl, Validators } from '@angular/forms';
 
-// describe('ErrorFormComponent', () => {
-//   let component: ErrorFormComponent;
-//   let fixture: ComponentFixture<ErrorFormComponent>;
+describe('ErrorFormComponent', () => {
+  let component: ErrorFormComponent;
+  let fixture: ComponentFixture<ErrorFormComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ErrorFormComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  const control: FormControl = new FormControl('',
+    [
+      Validators.required,
+      Validators.max(250),
+      Validators.min(10),
+      Validators.pattern('[+-]?([0-9]*[.])?[0-9]+')
+    ]);
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ErrorFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MatFormFieldModule],
+      declarations: [ ErrorFormComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ErrorFormComponent);
+    component = fixture.componentInstance;
+    component.control = control;
+    component.control.reset();
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('mat-error should no has error context', () => {
+    expect((fixture.nativeElement as HTMLElement).textContent).toBe('');
+  });
+
+  it('mat-error should have max error context', () => {
+    component.control.setValue(300);
+    fixture.detectChanges();
+    expect((fixture.nativeElement as HTMLElement).textContent).toContain('Wartość nie może być większa');
+  });
+
+  it('mat-error should have min error context', () => {
+    component.control.setValue(0);
+    fixture.detectChanges();
+    expect((fixture.nativeElement as HTMLElement).textContent).toContain('Wartość nie może być mniejsza');
+  });
+
+  it('mat-error should have pattern error context', () => {
+    component.control.setValue('test');
+    fixture.detectChanges();
+    expect((fixture.nativeElement as HTMLElement).textContent).toContain('Wartość musi być cyfrą');
+  });
+
+  it('mat-error should have require error context', () => {
+    component.control.markAsTouched();
+    fixture.detectChanges();
+    expect((fixture.nativeElement as HTMLElement).textContent).toContain('Pole jest obowiązkowe!');
+  });
+
+});
